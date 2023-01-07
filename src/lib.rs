a52ca42b50f25b8432ca04404ca5997e85564972
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-01-07 18:10:11 +0100

    refactor: move all library stuff out of lib.rs into util

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,2 @@
-use piet_common::{kurbo::RoundedRect, RenderContext};
-use std::error::Error;
-
-pub mod util;
+mod util;
 pub use util::*;
-
-pub const BASE_RESOLUTION: u32 = 4096;
-pub const BASE_ASPECT_RATIO: f64 = 5. / 7.2;
-pub const ROWS: u32 = 7;
-pub const COLUMNS: u32 = 10;
-
-pub trait DrawableCard {
-    fn index(&self) -> u32;
-
-    fn draw(&self, ctx: &mut impl RenderContext, area: &RoundedRect);
-
-    fn draw_back(&self, ctx: &mut impl RenderContext, area: &RoundedRect);
-}
-
-pub trait Import<T: DrawableCard> {
-    fn import(&mut self) -> Result<Vec<T>, Box<dyn Error>>;
-}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,24 @@
+use piet_common::{kurbo::RoundedRect, RenderContext};
+use std::error::Error;
+
 pub mod deck;
 pub mod device;
 pub mod dimensions;
 pub mod export;
+
+pub const BASE_RESOLUTION: u32 = 4096;
+pub const BASE_ASPECT_RATIO: f64 = 5. / 7.2;
+pub const ROWS: u32 = 7;
+pub const COLUMNS: u32 = 10;
+
+pub trait DrawableCard {
+    fn index(&self) -> u32;
+
+    fn draw(&self, ctx: &mut impl RenderContext, area: &RoundedRect);
+
+    fn draw_back(&self, ctx: &mut impl RenderContext, area: &RoundedRect);
+}
+
+pub trait Import<T: DrawableCard> {
+    fn import(&mut self) -> Result<Vec<T>, Box<dyn Error>>;
+}
